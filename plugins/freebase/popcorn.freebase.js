5b3d25dd4431325f607b75bf83271a6cca430b5e
Kyle Morton <kyle.morton@gmail.com>
2011-10-26 14:46:47 -0400

    first checkin of Freebase plugin
    This plugin uses JQuery templates and a Freebase experimental UI to
    fetch and render data. It lacks an example, unit tests and a license.

---
+++ plugins/freebase/popcorn.freebase.js
@@ -0,0 +1,162 @@
+// PLUGIN: FREEBASE
+
+(function ( Popcorn ) {
+  
+  /**
+   * Freebase popcorn plug-in 
+   * Displays topic title, description and thumbnail
+   * uses experimental JSON interface from Freebase and JSONP jQuery implementation
+   * http://www.freebase.com/experimental/topic/standard/[LANG]/[TOPIC]
+   *
+   * The plugin is based on the wikipedia plugin example
+   * 
+   * Example:
+     var p = Popcorn("#video")
+        .freebase({
+          start: 5, // seconds
+          end: 15, // seconds
+          topic: "queen_latifah",
+          target: "wikidiv"
+        } )
+   *
+   */
+   
+   
+  Popcorn.plugin( "freebase" , {
+      
+    manifest: {
+      about:{
+        name: "Popcorn Freebase Plugin",
+        version: "0.1",
+        author: "@kylemorton",
+        website: "www.ramp.com"
+      },
+      options:{
+        start: {
+          elem: "input", 
+          type: "text", 
+          label: "In"
+        },
+        end: {
+          elem: "input", 
+          type: "text", 
+          label: "Out"
+        },
+        topic: {
+          elem: "input", 
+          type: "text", 
+          label: "freebase topic key"
+        },
+        lang: {
+          elem: "input", 
+          type: "text", 
+          label: "Language"
+        },
+        callback: {
+          elem: "input", 
+          type: "function", 
+          label: "handles results"
+        },
+        template: {
+          elem: "input", 
+          type: "text", 
+          label: "jQuery template object"
+        },
+        target: "freebase-container",
+        debug: false
+      }
+    },
+    
+    supportsExperienceManager: true,
+    _data: {},
+
+    _setup : function( options ) {
+        if (options.debug) {
+            console.log("popcorn.freebase._setup")
+            console.log(options)
+        }
+        
+        //if a template is used then load template prerequisites
+        if (options.template) { 
+            Popcorn.getScript("http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js");
+            Popcorn.getScript("http://ajax.microsoft.com/ajax/jquery.templates/beta1/jquery.tmpl.min.js");
+        }
+    
+    
+      // declare needed variables
+      // get a guid to use for the global freebasecallback function
+      var  _text, _guid = Popcorn.guid(); 
+      
+      // if the user didn't specify a language default to english
+      if ( !options.lang ) { 
+        options.lang = "en"; 
+      }
+
+      //make sure the topic is formatted properly
+      options.topic =  String(options.topic.split(' ').join('_')).toLowerCase();
+
+      
+      //preload
+      var service_url = "http://www.freebase.com/experimental/topic/standard" + "/" + options.lang + "/" + options.topic;
+      if (options.debug)
+        console.log("popcorn.freebase.service_url = " + service_url);
+
+        var _out = this;
+       Popcorn.getJSONP(service_url + "?callback=jsonp", function(response) {
+            var result = response.result;
+            options._result = response.result;
+            options._fired = true;
+
+            _out._data = options._result;
+        });
+
+
+    },
+
+    start: function( event, options ){
+      // dont do anything if the information didn't come back from freebase
+      var isReady = function () {
+        
+        if ( !options._fired ) {
+          setTimeout( function () {
+            isReady();
+          }, 13);
+        } else {
+      
+            if (options.callback) 
+                options.callback({event:"end",topic:options.topic,result:options._result});
+
+            if (options.template) {
+                    for (var i=0; i < options.template.length; i++) {
+                        var t = options.template[i];
+                        t.data = options._result;
+                                            
+                        if (t.prop) {
+                            var props = options._result.properties[t.prop].values;
+                            t.props = props;                    
+                        }
+                        
+                        $( "#"+ t.name ).tmpl( t ).appendTo( $("#" + options.target) );                        
+                    }
+            }
+            
+
+        }
+      };
+      
+      isReady();
+    },
+
+    end: function( event, options ){
+        if (options.callback) 
+            options.callback({event:"end",topic:options.topic,result:options._result});
+
+        $("#" + options.target).html("");
+    },
+
+    _teardown: function( options ){
+
+    }
+  });
+
+})( Popcorn );
